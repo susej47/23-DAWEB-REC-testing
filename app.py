c8b31085228c7ba091e6afa5fab99f4fde1ceef4
clases <clases@Servidor.localdomain>
2024-04-18 09:29:53 +0200

    modifico el app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ def suma(a: int, b: int) -> int:
     """ docstring function
     # debe dar errores en linter
     """
+    w = "no se usa"
     x = "no se usa"
     y = "no se usa"
     z = "no se usa"
